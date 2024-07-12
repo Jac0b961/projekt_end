174ced7423fb60baaf4fdd55b29dd902d21745f4
Jac0b961 <opona9611@gmail.com>
2024-07-12 23:16:21 +0200

    Task2: Wczytywanie i weryfikacja pliku JSON

--- projekt.py
+++ projekt.py
@@ -1,5 +1,6 @@
 # project.py
 import sys
+import json
 
 def parse_arguments():
     if len(sys.argv) != 3:
@@ -9,7 +10,25 @@ def parse_arguments():
     output_file = sys.argv[2]
     return input_file, output_file
 
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        print(f"Loaded data: {data}")
+        return data
+    except json.JSONDecodeError:
+        print("Invalid JSON format")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+
 if __name__ == "__main__":
     input_file, output_file = parse_arguments()
+    if input_file.endswith('.json'):
+        data = load_json(input_file)
+    else:
+        print("Unsupported file format")
+        sys.exit(1)
     print(f"Input file: {input_file}")
     print(f"Output file: {output_file}")
